5376a736457e1a3e7417a558175d3a2ef345c47d
Graham Lally <graham.lally@ocsi.co.uk>
2016-07-15 16:51:07 +0100

    Still some debug info in there, but a call to /configure on the client host should now actually write files based on the info it gets from the configured server. Callooh! Callay!

--- httpdocs/configure.php
+++ httpdocs/configure.php
@@ -38,3 +38,49 @@ echo json_encode(array(
     'success' => true,
     'body' => $response->body // body stored as JSON format string, not JSON object
 ));
+
+$body = json_decode($response->body, true);
+
+foreach ($body as $file_id => $file_config) {
+    
+    $file_info = $file_config['file_info'];
+    $file_vars = $file_config['config_vars'];
+    
+    $host_config = new HostConfig();
+    // Add dummy output section for now
+    $host_config_section = new HostConfigSection('config'); 
+    $host_config_options = array();
+    
+    // Set up HostConfig values
+    foreach ($file_vars as $var_id => $var_info) {
+        
+        $config_option = new ConfigOption(
+            $var_info['var_key'],
+            $var_info['var_value'],
+            $var_info['add_quotes'],
+            $var_info['comment'],
+            $var_info['allow_client_override']
+        );
+        
+        $host_config_section->addConfigOption($config_option);
+    }
+
+    // Add dummy output section to host config for this file
+    $host_config->addHostConfigSection($host_config_section);
+    
+    // Set up file writer
+    if ($file_info['file_type'] == 'php') {
+        $file_writer = new PhpHostConfigWriter(
+                                $file_info['file_name'], 
+                                $file_info['file_path'], 
+                                $host_config,
+                                'PhpHostConfigSectionWriter', 
+                                'PhpConfigOptionWriter'
+                           );
+        $file_writer->setOverWriteExistingFile(true);
+                           
+        print_r($file_writer->getContent());
+        $file_writer->write();
+    }
+    
+}
