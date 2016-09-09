6cc6e497782ee7c3b2f3feff8691e6b13972a0d4
Graham Lally <graham.lally@ocsi.co.uk>
2016-09-09 15:30:39 +0100

    Configure.php takes domain from config file instead of server host

--- httpdocs/configure.php
+++ httpdocs/configure.php
@@ -7,7 +7,7 @@ $client_config = $client_config_file->getConfigerClientConfig();
 
 $cfg_source_host = $client_config->getConfigValue('host');
 $token = $client_config->getConfigValue('token');
-$requesting_host = $_SERVER['HTTP_HOST'];
+$requesting_host = $client_config->getConfigValue('local_domain');
 
 $remote_server = new RemoteConfigerServerConnection(
                             $cfg_source_host,
